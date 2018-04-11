895b0d6d1fcdfe8c4e8f88155eae3d3581528306
htao <htao@thoughtworks.com>
2018-04-11 11:13:51 +0800

    [taohui] init spring boot application and create root router

---
+++ src/main/java/com/thoughtworks/school/application.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class application {
+
+    @RequestMapping("/")
+    String home(){
+        return "my home";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(application.class, args);
+    }
+}
